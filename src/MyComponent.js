42843a73694e2bbbf1eeee411b15b9071a69dec1
kbh <bohyun611.kim@gmai.com>
2022-02-23 16:14:04 +0900

    비구조화 props 추가 테스트

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 const MyComponent = (props) => {
     console.log(props);
-    const { name, children } = props;
+    const { name, children, age, sex } = props;
     return (
         <div>
             my name is {name} <br />
-            nickname is {children}
+            nickname is {children} <br />
+            단비 : {age} 이고 {sex}
         </div>
     );
 };
@@ -14,6 +15,8 @@ const MyComponent = (props) => {
 // 기본값 설정
 MyComponent.defaultProps = {
     name: '기본이름',
+    age: 12,
+    sex: 'man',
 };
 
 export default MyComponent;
