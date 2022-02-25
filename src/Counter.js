6dfe2871386c88fc5979087a34d9e0fc2a2e796f
kbh <bohyun611.kim@gmai.com>
2022-02-25 11:35:08 +0900

    state 실습3 상태가 불변으로 고정 실습

--- src/Counter.js
+++ src/Counter.js
@@ -6,16 +6,18 @@ class Counter extends Component {
         // 초기값 설정
         this.state = {
             number: 0,
+            fixedNumber: 0,
         };
     }
 
     render() {
         console.log(this.state);
-        const { number } = this.state;
+        const { number, fixedNumber } = this.state;
         return (
             <div>
                 <h1>STATE DEMO </h1>
-                <h1>{number}</h1>
+                <h1>상태가 변하는 값 : {number}</h1>
+                <h2>바뀌지않는 값: {fixedNumber}</h2>
                 <button
                     onClick={() => {
                         this.setState({ number: number + 1 });
