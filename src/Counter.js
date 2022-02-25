3cbd4178580b58cddd3f3dc24d1ae57487d7338a
kbh <bohyun611.kim@gmai.com>
2022-02-25 11:43:25 +0900

    state 실습4  this.setState

--- src/Counter.js
+++ src/Counter.js
@@ -1,37 +1,29 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        // 초기값 설정
-        this.state = {
-            number: 0,
-            fixedNumber: 0,
-        };
-    }
+    // constructor에서 꺼내기
+    state = {
+        number: 0,
+        fixedNumber: 0,
+    };
 
     render() {
         console.log(this.state);
         const { number, fixedNumber } = this.state;
         return (
             <div>
-                <h1>STATE DEMO </h1>
                 <h1>상태가 변하는 값 : {number}</h1>
                 <h2>바뀌지않는 값: {fixedNumber}</h2>
                 <button
                     onClick={() => {
-                        this.setState({ number: number + 1 });
+                        this.setState((prevState) => {
+                            return {
+                                number: prevState.number + 1,
+                            };
+                        });
                     }}
                 >
                     <span>+1</span>
-                </button>
-
-                <button
-                    onClick={() => {
-                        this.setState({ number: number - 1 });
-                    }}
-                >
-                    <span>-1</span>
                 </button>
             </div>
         );
