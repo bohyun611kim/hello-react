1d6ff9eed79402b849e6ff2348f39491c160148f
kbh <bohyun611.kim@gmai.com>
2022-02-25 17:51:29 +0900

    이벤트 실험

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -5,30 +5,33 @@ class EventPractice extends Component {
     state = {
         messsage: '',
     };
+
+    // 생성자 가독성을 높여보기
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    // 핸들러
+    handleChange(e) {
+        console.log(e.target.value);
+        this.setState({
+            messsage: e.target.value,
+        });
+    }
+
+    handleClick() {
+        alert(this.state.messsage);
+        this.setState({ messsage: '' });
+    }
+
     render() {
         return (
             <div>
                 <h1>이벤트 연습</h1>
-                <input
-                    type="text"
-                    name="message"
-                    placeholder="이메일을 넣으세요"
-                    value={this.state.messsage}
-                    onChange={(e) => {
-                        console.log(e.target.value);
-                        this.setState({ messsage: e.target.value });
-                    }}
-                />
-                <button
-                    onClick={() => {
-                        alert(this.state.messsage);
-                        this.setState({
-                            messsage: '',
-                        });
-                    }}
-                >
-                    확인
-                </button>
+                <input type="text" name="message" value={this.state.messsage} onChange={this.handleChange} />
+                <button onClick={this.handleClick}>확인</button>
             </div>
         );
     }
