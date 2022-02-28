20518c3b8e14fd3666d0b3fbb79530f1a4f6d2c0
kbh <bohyun611.kim@gmai.com>
2022-02-28 10:46:11 +0900

    이벤트 key: valye 설정

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -3,27 +3,32 @@ import React, { Component } from 'react';
 class EventPractice extends Component {
     // 상태값 설정
     state = {
+        username: '',
         messsage: '',
     };
 
     // 핸들러
     handleChange = (e) => {
-        console.log(e.target.value);
         this.setState({
-            messsage: e.target.value,
+            [e.target.name]: e.target.value,
         });
     };
 
     handleClick = () => {
-        alert(this.state.messsage);
-        this.setState({ messsage: '' });
+        alert(this.state.username + ': ' + this.state.messsage);
+        this.setState({
+            username: '',
+            messsage: '',
+        });
     };
 
     render() {
         return (
             <div>
-                <h1>이벤트 연습</h1>
-                <input type="text" name="message" value={this.state.messsage} onChange={this.handleChange} />
+                <h1>이벤트 연습</h1> <hr />
+                <input type="text" name="username" placeholder="이름" value={this.state.username} onChange={this.handleChange}></input>
+                <br />
+                <input type="text" name="messsage" placeholder="아무거나" value={this.state.messsage} onChange={this.handleChange}></input>
                 <button onClick={this.handleClick}>확인</button>
             </div>
         );
